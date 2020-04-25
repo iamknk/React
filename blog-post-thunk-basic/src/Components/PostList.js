8566616977b54230cb711e9c84521a868dec9e96
Kashif Nazir Khan <pb193105@gmail.com>
2020-04-25 23:38:27 +0500

    blog thunk app completed

--- blog-post-thunk-basic/src/Components/PostList.js
+++ blog-post-thunk-basic/src/Components/PostList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../actions";
+import { fetchPosts, fetchPostsAndUsers } from "../actions";
 import UserHeader from "./UserHeader";
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
   renderList() {
     return this.props.posts.map((post) => {
@@ -30,4 +30,4 @@ class PostList extends React.Component {
 const mapStateToProps = (state) => {
   return { posts: state.posts };
 };
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
--- blog-post-thunk-basic/src/actions/index.js
+++ blog-post-thunk-basic/src/actions/index.js
@@ -1,4 +1,11 @@
+import _ from "lodash";
 import jsonPlaceholder from "../api/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => {
   return async (dispatch) => {
