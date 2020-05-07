e33f5532d1245441f05e01fd3e5e4ece92dfca61
Kashif Nazir Khan <pb193105@gmail.com>
2020-05-07 20:51:53 +0500

    actions and reducers etup

--- stream-video-project/client/src/actions/index.js
+++ stream-video-project/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 import { actionTypes } from "redux-form";
 
 export const signIn = (userId) => {
@@ -18,5 +26,33 @@ export const createStream = (formValues) => {
   return async (dispatch) => {
     const response = await streams.post("/streams", formValues);
     dispatch({ type: CREATE_STREAM, payload: response.data });
+  };
+};
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get("/streams");
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+  };
+};
+
+export const fetchSteam = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/stream/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+  };
+};
+
+export const editStream = (id, formValues) => {
+  return async (dispatch) => {
+    const response = streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });
   };
 };
--- stream-video-project/client/src/components/streams/StreamList.js
+++ stream-video-project/client/src/components/streams/StreamList.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StremList</div>;
-};
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  render() {
+    return <div>Heeey</div>;
+  }
+}
 
-export default StreamList;
+export default connect(null, fetchStreams)(StreamList);
