48835427fad83363ffeafc91a6374903784ff356
Kashif Nazir Khan <pb193105@gmail.com>
2020-05-13 23:09:36 +0500

    modal created

---
+++ stream-video-project/client/src/components/Modal.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import history from "../history";
+
+const Modal = () => {
+  return ReactDOM.createPortal(
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          Are You Sure you want to Delete this stream?
+        </div>
+        <div className="actions">
+          <button className="ui button primary">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
+export default Modal;
--- stream-video-project/client/src/components/streams/StreamDelete.js
+++ stream-video-project/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Modal from "../Modal";
 
 const StreamDelete = () => {
-  return <div>StremDelete</div>;
+  return (
+    <div>
+      Stream Delete
+      <Modal />
+    </div>
+  );
 };
 
 export default StreamDelete;
