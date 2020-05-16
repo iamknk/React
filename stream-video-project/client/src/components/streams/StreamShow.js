56142760f400c28ef7f324ea8896eface408cc23
Kashif Nazir Khan <pb193105@gmail.com>
2020-05-16 23:46:01 +0500

    live streaming using obs and node server completed

--- stream-video-project/client/src/components/streams/StreamShow.js
+++ stream-video-project/client/src/components/streams/StreamShow.js
@@ -1,10 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+  buildPlayer() {
+    const { id } = this.props.match.params;
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
   render() {
     if (!this.props.stream) {
@@ -13,6 +38,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
