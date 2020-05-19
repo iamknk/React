dc70fc78a111d9621e21d5062bc2c1b6acfaa553
Kashif Nazir Khan <pb193105@gmail.com>
2020-05-19 18:28:15 +0500

    context via consumer

--- translate-context/src/components/Button.js
+++ translate-context/src/components/Button.js
@@ -2,10 +2,14 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorlagan";
-    return <div className="ui button primary">{text}</div>;
+    return (
+      <div className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => (value === "english" ? "Submit" : "Voorlagan")}
+        </LanguageContext.Consumer>
+      </div>
+    );
   }
 }
 export default Button;
