9ac1a93cbc20df74b3842911cebf6f98a6998d37
Kashif Nazir Khan <pb193105@gmail.com>
2020-05-21 04:13:23 +0500

    hooks completed

---
+++ hooks-simple/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import ResourceList from "./ResourceList";
+const App = () => {
+  const [resource, setResource] = useState("posts");
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource("posts")}>Post</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
+export default App;
---
+++ hooks-simple/src/components/ResourceList.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+  const fetchResource = async (resource) => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${resource}`
+    );
+    setResources(response.data);
+  };
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
+
+  return (
+    <ul>
+      {resources.map((record) => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ResourceList;
